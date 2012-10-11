7e7893b2f3185e7bb27554aa1dcdc73aa6949ead
David Ketcheson <dketch@gmail.com>
2012-10-11 22:56:32 +0300

    Add nbconv.sh and two new posts.

---
+++ nbconv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+fname=$1
+
+nbconvert.py ${fname}.ipynb -f markdown
+sed  -i '' "s#${fname}_files#https:\/\/dl.dropbox.com\/u\/656693\/jekyll_images\/${fname}_files#g"  ${fname}.md
+
+dt=$(date "+%Y-%m-%d")
+echo $dt
+
+#sed -i '' 
+echo "0a
+---
+layout:    post
+time:      ${dt}
+title:     TITLE-ME
+subtitle:  SUBTITLE-ME
+tags:      TAG-ME
+---
+.
+w" | ed ${fname}.md
+
+mv ${fname}.md ~/labnotebook/_posts/${dt}-${fname}.md
