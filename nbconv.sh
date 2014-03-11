5fe3e030c867f9b252be4c94ee5d3bc351c08be4
David Ketcheson <dketch@gmail.com>
2014-03-11 22:08:19 +0300

    Post about DMDA.
    Former-commit-id: 277f726937361edf4a9f56bed8b2b7594e297edb

--- nbconv.sh
+++ nbconv.sh
@@ -2,7 +2,7 @@
 
 fname=$1
 
-nbconvert.py ${fname}.ipynb -f markdown
+ipython nbconvert --to markdown ${fname}.ipynb
 sed  -i '' "s#${fname}_files#https:\/\/dl.dropbox.com\/u\/656693\/jekyll_images\/${fname}_files#g"  ${fname}.md
 
 dt=$(date "+%Y-%m-%d")
@@ -18,4 +18,4 @@ tags:      TAG-ME
 .
 w" | ed ${fname}.md
 
-mv ${fname}.md ~/labnotebook/_posts/${dt}-${fname}.md
+mv ${fname}.md ~/Research/labnotebook/_posts/${dt}-${fname}.md
