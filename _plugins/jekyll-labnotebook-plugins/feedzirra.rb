b399206ebd8e7a0e79e79f3dafc9bbb7bd3f9e01
David Ketcheson <dketch@gmail.com>
2014-10-17 11:59:53 +0300

    Update plugins.
    Former-commit-id: a76ee050680a24148329a066cc42d326b3006578

--- _plugins/jekyll-labnotebook-plugins/feedzirra.rb
+++ _plugins/jekyll-labnotebook-plugins/feedzirra.rb
@@ -1,12 +1,12 @@
-require 'feedzirra'
+require 'feedjira'
 
 # Need to store feed in cache and just call update?
 
 module Jekyll
 
-  class FeedzirraBlockTag < Liquid::Tag
+  class FeedjiraBlockTag < Liquid::Tag
     def render(context)
-      feed = Feedzirra::Feed.fetch_and_parse("https://github.com/dketch.atom")
+      feed = Feedjira::Feed.fetch_and_parse("https://github.com/dketch.atom")
       # consider formatting properly
       feed.title + feed.entries[0].content + feed.entries[1].content + feed.entries[2].content + feed.entries[3].content 
     end
@@ -14,5 +14,5 @@ module Jekyll
 
 end
 
-Liquid::Template.register_tag('feedzirra', Jekyll::FeedzirraBlockTag)
+Liquid::Template.register_tag('feedjira', Jekyll::FeedjiraBlockTag)
 
