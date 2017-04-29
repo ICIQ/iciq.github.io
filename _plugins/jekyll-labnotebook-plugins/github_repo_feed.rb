dbec007c7fdfdb05b79ad91191075a30c10fe1c3
Xiaodong Qi <qxd@unm.edu>
2017-04-28 23:32:18 -0600

    Start using Trusty platform and hide the feedjira warning message. Ref: https://github.com/feedjira/feedjira/issues/380

--- _plugins/jekyll-labnotebook-plugins/github_repo_feed.rb
+++ _plugins/jekyll-labnotebook-plugins/github_repo_feed.rb
@@ -2,23 +2,25 @@ require 'feedjira' # atom/rss API
 require 'octokit'   # Github API
 require 'nokogiri'  # Parse HTML
 
-# Github feed based on feedzirra rather than the Github API
+# Github repo commit feed based on feedzirra rather than the Github API.
+# By Xiaodong Qi, based on the github_feed.rb plugin.
 
 module Jekyll
-  class GithubFeed < Liquid::Tag
+  class GithubRepoFeed < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
       input = text.split(/, */ )
       @user = input[0]
-      @count = input[1]
-      if input[1] == nil
+      @repo = input[1]
+      @count = input[2]
+      if input[2] == nil
         @count = 3
       end
     end
     def render(context)
       Feedjira.logger.level = Logger::FATAL # Hide log of warning. See https://github.com/feedjira/feedjira/issues/380 also the earlier warning message: https://travis-ci.org/i2000s/en/builds/207516706
-      puts "Generating Github feed from rss using feedzirra with github_feed.rb"
-      feed = Feedjira::Feed.fetch_and_parse("https://github.com/" + @user + ".atom")
+      puts "Generating Github repo feed from rss using feedzirra with github_repo_feed.rb"
+      feed = Feedjira::Feed.fetch_and_parse("https://github.com/" + @user + "/" + @repo + "/commits.atom") # This will call the rss from the repo's default branch.
       # consider formatting properly
       if defined?(feed.entries)
         out = "<ul>"
@@ -42,4 +44,4 @@ module Jekyll
   end
 end ## end module Jekyll
 
-Liquid::Template.register_tag('github_feed', Jekyll::GithubFeed)
+Liquid::Template.register_tag('github_repo_feed', Jekyll::GithubRepoFeed)
